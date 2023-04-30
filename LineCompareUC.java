3e90d766e79db66284c261a74c70389a23f4d786
vishlesha02 <vishleshamortale18@gmail.com>
2023-04-30 23:44:32 +0530

    LineCompareUC

--- LineCompareUC.java
+++ LineCompareUC.java
@@ -3,43 +3,52 @@ import java.util.Scanner;
 
 public class LineCompareUC {
 
-        public static void main(String[] args) {
-            double sum1=0 , sum2=0;
-            Scanner sc = new Scanner(System.in);
-            for (int i = 1; i <= 2 ; i++) {
-                System.out.println("Enter the value for Line "+ i);
-                System.out.print("Enter value of x1 = ");
-                int x1 = sc.nextInt();
+            public static void compare(double L1, double L2){
+                if (L1==L2){
+                    System.out.println("Line1 = " + L1 );
+                    System.out.println("Line2 = " + L2);
+                    System.out.println("Line 1 and Line 2 line are equal");
 
-                System.out.print("Enter value of x2 = ");
-                int x2 = sc.nextInt();
-
-                System.out.print("Enter value of y1 = ");
-                int y1 = sc.nextInt();
-
-                System.out.print("Enter value of y2 = ");
-                int y2 = sc.nextInt();
-
-                double x, y;
-                x = (x2 - x1) * (x2 - x1);
-                y = (y2 - y1) * (y2 - y1);
-
-                if (i==1)
-                {
-                    sum1 = Math.sqrt(x + y);
+                } else if (L1>L2) {
+                    System.out.println("Line1: " + L1);
+                    System.out.println("Line2: " + L2);
+                    System.out.println("Line 1 is greater tha Line 2");
                 }
                 else{
-                    sum2 = Math.sqrt(x + y);
+                    System.out.println("Line1: " + L1);
+                    System.out.println("Line2: " + L2);
+                    System.out.println("Line 2 is greater than Line 1");
                 }
             }
-            String sumofLine1= String.valueOf(sum1);
-            String sumofLine2= String.valueOf(sum2);
-            if (sumofLine1.equals(sumofLine2)) {
-                System.out.println("Both lines are equal");
+            public static void main(String[] args) {
+                double sum1=0 , sum2=0;
+                Scanner sc = new Scanner(System.in);
+                for (int i = 1; i <= 2 ; i++) {
+                    System.out.println("Enter the value for Line "+ i);
+                    System.out.print("Enter value of x1 = ");
+                    int x1 = sc.nextInt();
+
+                    System.out.print("Enter value of x2 = ");
+                    int x2 = sc.nextInt();
+
+                    System.out.print("Enter value of y1 = ");
+                    int y1 = sc.nextInt();
+
+                    System.out.print("Enter value of y2 = ");
+                    int y2 = sc.nextInt();
+                    double x, y;
+                    x = (x2 - x1) * (x2 - x1);
+                    y = (y2 - y1) * (y2 - y1);
+
+                    if (i==1)
+                    {
+                        sum1 = Math.sqrt(x + y);
+                    }
+                    else{
+                        sum2 = Math.sqrt(x + y);
+                    }
+                }
+
+                compare(sum1,sum2);
             }
-            else
-            {
-                System.out.println("Both lines are not equal");
-            }
-        }
 }
